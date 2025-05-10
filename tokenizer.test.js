2b20fbd155a9006eeec048d4c89c3e68f8403621
darthspectrum <muradianator@gmail.com>
2025-05-11 01:48:21 +0400

    :construction: setup for step 03

--- tokenizer.test.js
+++ tokenizer.test.js
@@ -1,5 +1,5 @@
-import { tokenize, typeify } from "./sintez.js";
-import { assertEquals } from "jsr:@std/assert";
+import { tokenize } from "./sintez.js";
+import { assertEquals, fail } from "jsr:@std/assert";
 
 const atom = (name) => Symbol.for(name);
 
@@ -45,5 +45,20 @@ Deno.test("Tokenizer", async (t) => {
     },
   });
 
+  await t.step({
+    name: "tokenize a list",
+    fn: () => {
+      const result = tokenize("(a 1)");
+      assertEquals(result, [[atom("a"), 1]]);
+    },
+  });
 
+  await t.step({
+    name: "tokenize a nested list",
+    fn: () => {
+      fail(
+        "This test is not implemented yet. Please implement it.",
+      );
+    },
+  });
 });
